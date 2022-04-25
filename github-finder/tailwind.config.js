98dc5fa27c20ef23952bd73bb129565f872dafc8
diazAimar <federico.diaz@est.fi.uncoma.edu.ar>
2022-04-25 16:03:12 -0300

    Agregado de las estadisticas de los usuarios.

--- github-finder/tailwind.config.js
+++ github-finder/tailwind.config.js
@@ -1,10 +1,10 @@
 module.exports = {
-	content: ['./src/**/*.{js,jsx,ts,tsx}'],
-	theme: {
-		extend: {},
-	},
-	plugins: [require('daisyui')],
-	daisyui: {
-		themes: ['dark'],
-	},
+  content: ["./src/**/*.{js,jsx,ts,tsx}"],
+  theme: {
+    extend: {},
+  },
+  plugins: [require("daisyui")],
+  daisyui: {
+    themes: ["dark"],
+  },
 };
