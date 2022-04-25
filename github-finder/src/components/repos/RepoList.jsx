900b905a3dd70bc688eade6828185eb14f52243a
diazAimar <federico.diaz@est.fi.uncoma.edu.ar>
2022-04-25 17:06:07 -0300

    Creacion de RepoItem.js y agregado de informacion de los repositorios, para una mejor renderizacion de los mismos en la lista. (Clase 65: Repo Items).

--- github-finder/src/components/repos/RepoList.jsx
+++ github-finder/src/components/repos/RepoList.jsx
@@ -1,4 +1,5 @@
 import PropTypes from 'prop-types';
+import RepoItem from './RepoItem';
 
 RepoList.propTypes = {
   repos: PropTypes.array.isRequired,
@@ -10,7 +11,7 @@ export default function RepoList({ repos }) {
       <div className="card-body">
         <h2 className="text-3xl my-4 font-bold card-title">Latest Repositories</h2>
         {repos.map((repo) => (
-          <h3>{repo.name}</h3>
+          <RepoItem key={repo.id} repo={repo} />
         ))}
       </div>
     </div>
