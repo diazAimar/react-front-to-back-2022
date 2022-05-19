b829e752c3c0482d1934726c96d5c1cb2a9d7994
diazAimar <federico.diaz@est.fi.uncoma.edu.ar>
2022-05-19 11:16:17 -0300

    Se creo un componente Slider.jsx que sera llamado desde Explore.jsx (o mejor dicho, nuestro home). Esto creara un slider el cual trae las ultimas 5 publicaciones, y usando el hook useNavigate nos permite dirigirnos hacia la publicacion especifica haciendo click en la imagen de la misma en el slider.

--- house-marketplace/src/pages/Explore.jsx
+++ house-marketplace/src/pages/Explore.jsx
@@ -1,13 +1,16 @@
 import { Link } from 'react-router-dom';
 import rentCategoryImage from '../assets/jpg/rentCategoryImage.jpg';
 import sellCategoryImage from '../assets/jpg/sellCategoryImage.jpg';
+import Slider from '../components/Slider';
 
 export default function Explore() {
   return (
     <div className="explore">
       <header></header>
       <p className="pageHeader">Explore</p>
-      <main>{/* Slider */}</main>
+      <main>
+        <Slider />
+      </main>
       <p className="exploreCategoryHeading">Categories</p>
       <div className="exploreCategories">
         <Link to="/category/rent">
